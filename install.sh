685c5840635d4dc8ad60f99cffbe1d44e35c060f
Nicolas Cantu <nicolas.cantu@00smartvillage.com>
2015-12-02 02:17:34 +0100

    order mkdir, cd :)

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ apt-get install nodejs
 git config --global user.email "nicolas.cantu@instriit.com"
 git config --global user.name "Nicolas Cantu"
 npm install -g bower
-cd /var/
-mkdir www
+mkdir /var/www
+cd /var/www
 wget https://github.com/PolymerElements/polymer-starter-kit/releases/download/v1.2.0/polymer-starter-kit-1.2.0.zip
 unzip polymer-starter-kit-1.2.0.zip
 cd polymer-starter-kit-1.2.0
